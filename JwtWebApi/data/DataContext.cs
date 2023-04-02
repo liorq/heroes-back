f4ffa5e125a33f42a8e7c309233e6807801d043d
lior daniel <liorqm@gmail.com>
2023-04-02 17:26:10 +0300

    first commit

--- JwtWebApi/data/DataContext.cs
+++ JwtWebApi/data/DataContext.cs
@@ -9,10 +9,7 @@ namespace JwtWebApi.data
         public DbSet<User> Users { get; set; }
         public DbSet<Hero> AllHeroes { get; set; }
 
-        public DbSet<Guests> Guests { get; set; }
-        public DbSet<Messages> Messages { get; set; }
-        public DbSet<Missions> Missions { get; set; }
-        public DbSet<Rooms> Rooms { get; set; }
+
 
     }
 }
